30fd2aa72974775fc9eb5cfceaa9842cc9d5ea92
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2019-03-18 19:46:49 +0300

    Queue<T>

--- SwiftGenerics_02.playground/Contents.swift
+++ SwiftGenerics_02.playground/Contents.swift
@@ -33,9 +33,7 @@ class List<T> : Container {
     }
     
     var count: Int {
-        get {
-            return size
-        }
+        return size
     }
     
     subscript(i: Int) -> T {
@@ -52,6 +50,11 @@ class List<T> : Container {
         }
     }
     
+    func removeFirstElement() {
+        first = first?.next
+        size -= 1
+    }
+    
     private class Node {
         var value: T?
         var next: Node?
@@ -62,6 +65,35 @@ class List<T> : Container {
     }
 }
 
+class Queue<T> : Container {
+    
+    private let list = List<T>()
+    
+    func append(_ item: T) {
+        list.append(item)
+    }
+    
+    var count: Int {
+        return list.count
+    }
+    
+    subscript(i: Int) -> T {
+        return list[i]
+    }
+    
+    func enqueue(_ item: T) {
+        append(item)
+    }
+    
+    func dequeue() -> T {
+        let val = list[0]
+        
+        list.removeFirstElement()
+        
+        return val
+    }
+}
+
 func integerListTest() {
     let intList = List<Int>()
     
@@ -69,14 +101,47 @@ func integerListTest() {
     intList.append(98)
     intList.append(4093)
     
-    print("integerListTest()")
+    print("BEGIN integerListTest()")
     print("intList.count = \(intList.count)")
     print("intList[0] = \(intList[0])")
     print("intList[1] = \(intList[1])")
     print("intList[2] = \(intList[2])")
+    
+    print("intList.removeFirstElement()")
+    intList.removeFirstElement()
+    print("intList[0] = \(intList[0])")
+    print("intList.count = \(intList.count)")
     // print("intList[3] = \(intList[3])")
+    print("END integerListTest()")
+}
+
+func queueTest() {
+    print("BEGIN queueTest()")
+    
+    let someQueue = Queue<String>()
+    
+    someQueue.enqueue("AA")
+    someQueue.enqueue("BB")
+    someQueue.enqueue("CC")
+    someQueue.enqueue("DD")
+    
+    print("someQueue.count = \(someQueue.count)")
+    
+    let val1 = someQueue.dequeue()
+    let val2 = someQueue.dequeue()
+    
+    someQueue.enqueue("EE")
+    
+    print("someQueue.dequeue() = \(val1)")
+    print("someQueue.dequeue() = \(val2)")
+    print("someQueue.dequeue() = \(someQueue.dequeue())")
+    print("someQueue.dequeue() = \(someQueue.dequeue())")
+    print("someQueue.dequeue() = \(someQueue.dequeue())")
+    print("someQueue.count = \(someQueue.count)")
+    print("END queueTest()")
 }
 
 integerListTest()
+queueTest()
 
 print("COMPLETED SUCCESFULLY.")
